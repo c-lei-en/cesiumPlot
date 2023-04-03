54c3480f782a87d6f0ad406d0e491d693ec8ec8f
c-lei-en <chenglei713@gmail.com>
2023-04-03 21:08:10 +0800

    透视分析，地形透视，后续将添加三维模型透视分析

---
+++ src/views/map/analysis/tools/VisibilityTool.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-card style="overflow: auto">
+    <el-button-group>
+      <el-button type="primary" text :icon="Edit" @click="visibilityClick"
+        >透视分析</el-button
+      >
+      <el-button
+        type="primary"
+        text
+        :icon="Delete"
+        @click="clearVisibilityClick()"
+        >清除</el-button
+      >
+    </el-button-group>
+  </el-card>
+</template>
+
+<script lang="ts" setup>
+import { Delete, Edit } from "@element-plus/icons-vue";
+import { VisibilityAnalysis } from "./visibilityTool";
+import { onUnmounted } from "vue";
+
+const visibilityTool = new VisibilityAnalysis();
+
+function visibilityClick() {
+  visibilityTool.analysisVisible();
+}
+
+function clearVisibilityClick() {
+  visibilityTool.clearAll();
+}
+
+onUnmounted(() => {
+  visibilityTool.destory();
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/map/analysis/tools/visibilityTool/index.ts
@@ -0,0 +1,126 @@
+import { getCatesian3FromPX } from "@/views/map/plot/tools";
+import {
+  CallbackProperty,
+  Cartesian3,
+  Color,
+  Entity,
+  PolylineGraphics,
+  Ray,
+  ScreenSpaceEventHandler,
+  ScreenSpaceEventType,
+  defined,
+} from "cesium";
+
+export class VisibilityAnalysis {
+  handler: any;
+  planeLineEntityList: any[];
+  pointList: any[];
+  constructor() {
+    this.planeLineEntityList = [];
+    this.pointList = [];
+    this.handler = new ScreenSpaceEventHandler(window.Viewer.scene.canvas);
+  }
+
+  // * 创建中间线条entity以适应动态数据
+  createLineEntity(isGround: boolean): Entity {
+    const update = () => {
+      return this.pointList;
+    };
+    return window.Viewer.entities.add({
+      polyline: new PolylineGraphics({
+        positions: new CallbackProperty(update, false),
+        show: true,
+        material: Color.BLUE,
+        clampToGround: isGround,
+      }),
+    });
+  }
+
+  // * 绘制线
+  drawLine(leftPoint: Cartesian3, secPoint: Cartesian3, color: Color) {
+    const line = window.Viewer.entities.add({
+      polyline: {
+        positions: [leftPoint, secPoint],
+        width: 1,
+        material: color,
+        depthFailMaterial: color,
+      },
+    });
+    this.planeLineEntityList.push(line);
+  }
+
+  analysisVisible() {
+    this.pointList = [];
+    let lineEntity: null | Entity;
+    // * 监测鼠标左击事件
+    this.handler.setInputAction((evt: any) => {
+      const cartesian = getCatesian3FromPX(evt.position);
+      if (!cartesian) return;
+      const num = this.pointList.length;
+      if (num == 1) {
+        this.pointList.push(cartesian.clone());
+      } else {
+        this.pointList.push(cartesian.clone());
+        window.Viewer.entities.remove(lineEntity);
+
+        // 计算射线的方向
+        const direction = Cartesian3.normalize(
+          Cartesian3.subtract(
+            this.pointList[1],
+            this.pointList[0],
+            new Cartesian3()
+          ),
+          new Cartesian3()
+        );
+        // 建立射线
+        const ray = new Ray(this.pointList[0], direction);
+        const result = window.Viewer.scene.globe.pick(ray, window.Viewer.scene); // 计算交互点，返回第一个
+
+        if (result !== undefined && result !== null) {
+          this.drawLine(result, this.pointList[0], Color.GREEN); // 可视区域
+          this.drawLine(result, this.pointList[1], Color.RED); // 不可视区域
+        } else {
+          this.drawLine(this.pointList[0], this.pointList[1], Color.GREEN);
+        }
+        this.destoryHandler();
+      }
+    }, ScreenSpaceEventType.LEFT_CLICK);
+
+    // * 监测鼠标移动事件
+    this.handler.setInputAction((evt: any) => {
+      const cartesian = getCatesian3FromPX(evt.endPosition);
+      if (!defined(cartesian)) return;
+
+      if (this.pointList.length == 2 && !lineEntity) {
+        lineEntity = this.createLineEntity(false);
+      }
+
+      this.pointList.pop();
+      this.pointList.push(cartesian);
+    }, ScreenSpaceEventType.MOUSE_MOVE);
+  }
+
+  //   * 清除销毁
+  destoryHandler() {
+    this.handler.removeInputAction(ScreenSpaceEventType.LEFT_CLICK);
+    this.handler.removeInputAction(ScreenSpaceEventType.MOUSE_MOVE);
+    this.handler.removeInputAction(ScreenSpaceEventType.RIGHT_CLICK);
+  }
+
+  //   * 清除所有已绘制图形
+  clearAll() {
+    this.destoryHandler();
+    this.planeLineEntityList.forEach((item) => {
+      window.Viewer.entities.remove(item);
+    });
+    this.planeLineEntityList = [];
+    this.pointList = [];
+  }
+
+  //   * 销毁实例
+  destory() {
+    this.clearAll();
+    this.handler.destroy();
+    this.handler = undefined;
+  }
+}
